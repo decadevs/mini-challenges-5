2d179cb941fb0b3aa033940446abbd850a1d8ef1
unknown <obrienules@gmail.com>
2020-05-22 12:03:56 +0100

    Refactor code
    Initialise colLength and rowLength in constructor.

--- src/rotten-tomatoes/SpoilTomatoes.js
+++ src/rotten-tomatoes/SpoilTomatoes.js
@@ -2,8 +2,8 @@ class SpoilTomatoes {
 
     constructor(grid) {
         this.grid = grid;
-        this.getAdjacents = this.getAdjacents.bind(this)
-        this.getRottenTomatoes = this.getRottenTomatoes.bind(this)
+        this.colLength = this.grid[0].length - 1;
+        this.rowLength = this.grid.length - 1;
     };
 
     getRottenTomatoes() {
@@ -12,30 +12,30 @@ class SpoilTomatoes {
         for (let row = 0; row < this.grid.length; row++) {
             for (let col = 0; col < this.grid[row].length; col++) {
                 if (this.grid[row][col] === 2) {
-                    spoiltTomatoesLocations.push([row, col])
+                    spoiltTomatoesLocations.push([row, col]);
                 }
                 if (this.grid[row][col] === 1) {
-                    numGoodTomatoe++
+                    numGoodTomatoe++;
                 }
             }
         }
         return { numGoodTomatoe, spoiltTomatoesLocations };
     }
 
-    getAdjacents(position, colLength, rowLength) {
+    getAdjacents(position) {
         const [row, column] = position;
         const topAdjacent = row === 0 ? false : [row - 1, column];
-        const rightAdjacent = column === colLength ? false : [row, column + 1];
-        const bottomAdjacent = row === rowLength ? false : [row + 1, column];
-        const leftAdjacent = column === 0 ? false : [row, column - 1]
+        const rightAdjacent = column === this.colLength ? false : [row, column + 1];
+        const bottomAdjacent = row === this.rowLength ? false : [row + 1, column];
+        const leftAdjacent = column === 0 ? false : [row, column - 1];
         return [topAdjacent, rightAdjacent, bottomAdjacent, leftAdjacent]
             .filter(Boolean)
-            .filter(([row, col]) => this.grid[row][col] === 1)
+            .filter(([row, col]) => this.grid[row][col] === 1) ;
     }
 
     getTimeOfSpread() {
 
-        const { spoiltTomatoesLocations, numGoodTomatoe } = this.getRottenTomatoes()
+        const { spoiltTomatoesLocations, numGoodTomatoe } = this.getRottenTomatoes();
 
         if (!spoiltTomatoesLocations.length) {
             return -1
@@ -44,26 +44,25 @@ class SpoilTomatoes {
         let counter = -1;
         let queue = [];
         const numOfBadTomatoes = spoiltTomatoesLocations.length;
-        queue.push(...spoiltTomatoesLocations)
+        queue.push(...spoiltTomatoesLocations);
 
         while (queue.length) {
-            counter++
+            counter++;
             let arrOfBadTomatoes = [...queue];
             queue.length = 0;
             for (let i = 0; i < arrOfBadTomatoes.length; i++) {
                 let badTomatoe = arrOfBadTomatoes[i];
                 if (!visited[badTomatoe]) {
                     visited[badTomatoe] = true;
-                    let [row,] = badTomatoe;
-                    const adjacentTomatoes = this.getAdjacents(badTomatoe, this.grid[row].length - 1, this.grid.length - 1);
-                    queue.push(...adjacentTomatoes)
-                    queue = queue.filter(a => !visited[a])
+                    const adjacentTomatoes = this.getAdjacents(badTomatoe);
+                    queue.push(...adjacentTomatoes);
+                    queue = queue.filter(a => !visited[a]);
                 }
             }
 
         }
 
-        return (Object.keys(visited).length < (numGoodTomatoe + numOfBadTomatoes)) ? -1 : counter
+        return (Object.keys(visited).length < (numGoodTomatoe + numOfBadTomatoes)) ? -1 : counter ;
     }
 
 }
