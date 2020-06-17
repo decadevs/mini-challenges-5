fe31338007ff877979facea0db9a94f674bd3819
jbchinonso <nwekejohnbosco@gmail.com>
2020-06-18 00:16:33 +0200

    all test passed

--- src/rotten-tomatoes/index.js
+++ src/rotten-tomatoes/index.js
@@ -1,55 +1,68 @@
-let minuetes = 0;
-let visited = [];
-  
-
 function rottenTomatoes(grid) {
-  let affted = [];
-  for (let row = 0; row < grid.length; row++) {
-      for (let column = 0; column < grid[row].length; column++) {
-          if (grid[row][column] === 2 && !JSON.stringify(visited).includes([row,column])) {
-              visited.push([row,column])
-              findFreshTomatoes(grid,row,column)
-          }
-      }
-      
-  }
- 
-  function findFreshTomatoes(graph, row, column) {
-      // check neigbour below    
-      if (row + 1 < graph.length && graph[row + 1][column] === 1) {
-          affted.push([row+1,column])
-      }
-      // check top neigbour
-      if (row - 1 >= 0 && graph[row - 1][column] === 1) {
-         affted.push([row-1,column])
-      }
+    let minuetes = 0;
+    let impossible = false
+    let affected = [];
+   findRottenTomatoes(grid)
 
-      // check right neigbour
-      if (column + 1 < graph[row].length && graph[row][column + 1] === 1) {
-          affted.push([row,column+1])
-      }
+   //this function looks for infected or rotten tomatoes and grab thier index
+    function findRottenTomatoes(grid) {
+        for (let row = 0; row < grid.length; row++) {
+            for (let column = 0; column < grid[row].length; column++) {
+                if (grid[row][column] === 2 ) {
+                    grid[row][column] = 0
+                    findVictims(grid, row, column)
+                }
+            }
 
-      // check left neigbour
-      if (column - 1 >= 0 && graph[row][column - 1] === 1) {
-          affted.push([row,column-1])
-      }
-      
-  }
+        }
+    
+     //This function possible victims of the infect and place all of them into an array called affected   
+    function findVictims(graph, row, column) {
+        // check top neigbour
+        if (row - 1 >= 0 && graph[row - 1][column] === 1) {
+            affected.push([row - 1, column])
+        }
 
- if(affted.length > 0){
-  for(let position of affted){
-      grid[position[0]][position[1]] = 2
-  }
-  minuetes++
-  rottenTomatoes(grid)
- }
+        // check right neigbour
+        if (column + 1 < graph[row].length && graph[row][column + 1] === 1) {
+            affected.push([row, column + 1])
+        }
 
- //check if it is impossible to infect all the tomatoes
-for(let array of grid){
-  if(array.includes(1)) return -1
+        // check neigbour below    
+        if (row + 1 < graph.length && graph[row + 1][column] === 1) {
+            affected.push([row + 1, column])
+        }
+
+        // check left neigbour
+        if (column - 1 >= 0 && graph[row][column - 1] === 1) {
+            affected.push([row, column - 1])
+        }
+
+        
+    }
+    //check if there are victims to infect and infect them
+    if (affected.length) {
+        for (let position of affected) {
+            grid[position[0]][position[1]] = 2 
+        }
+        minuetes++
+        affected = [];
+        findRottenTomatoes(grid)
+    }
+
+    //check if it is impossible to infect all the tomatoes
+    if (!affected.length) {
+        for (let array of grid) {
+            if (array.includes(1)) {
+                impossible = true
+                return
+            }
+        }
+    }
+
 }
 
-return minuetes
+  return impossible ?  -1 : minuetes          
 
 }
 
